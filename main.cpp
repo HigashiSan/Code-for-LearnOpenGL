9afc917c8ca8d27af6a4694d3a6cf467ffb30bb8
Kiritokojima <56297955+Kiritokojima@users.noreply.github.com>
2020-05-12 09:54:08 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,395 @@
+#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "Shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include "Camera.h"
+#include "Material.h"
+#include "LightDirectional.h"
+#include "LightPoint.h"
+#include "LightSpot.h"
+#include "Mesh.h"
+#include "Model.h"
+
+#pragma region Camera Declare
+//Instantiate Camera Class
+	Camera camera(glm::vec3(0, 0, 3.0f), glm::radians(-15.0f), glm::radians(180.0f), glm::vec3(0, 1.0f, 0));
+#pragma endregion
+
+#pragma region Light Declare
+	LightDirectional lightD (glm::vec3(15.0f, 15.0f, -15.0f), 
+		glm::vec3(glm::radians(45.0f),glm::radians(45.0f),0),
+		glm::vec3(3.0f,3.0f,3.0f));
+
+	LightPoint lightP0 (glm::vec3(-15.0f, -15.0f,-15.0f),
+		glm::vec3(glm::radians(45.0f), glm::radians(45.0f), 0),
+		glm::vec3(2.0f,2.0f, 2.0f));
+	LightPoint lightP1 (glm::vec3(0, 20.0f, 0),
+		glm::vec3(glm::radians(45.0f), glm::radians(45.0f), 0),
+		glm::vec3(2.0f, 2.0f, 2.0f));
+	LightPoint lightP2 (glm::vec3(0, 0, 20.0f),
+		glm::vec3(glm::radians(45.0f), glm::radians(45.0f), 0),
+		glm::vec3(2.0f, 2.0f, 2.0f));
+	LightPoint lightP3 (glm::vec3(20.0f, 20.0f, 20.0f),
+		glm::vec3(glm::radians(45.0f), glm::radians(45.0f), 0),
+		glm::vec3(2.0f, 2.0f, 2.0f));
+	LightSpot lightS (glm::vec3(0, 20.0f, 0.0f),
+		glm::vec3(glm::radians(90.0f),0, 0),
+		glm::vec3(2.0f, 2.0f, 2.0f));
+
+#pragma endregion
+
+#pragma region Model Data
+
+//OBJ
+float vertices[] = {
+	// positions          // normals           // texture coords
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+	-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+	-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+	 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+};
+
+glm::vec3 cubePositions[] = {
+  glm::vec3(0.0f,  0.0f,  0.0f),
+  glm::vec3(2.0f,  5.0f, -15.0f),
+  glm::vec3(-1.5f, -2.2f, -2.5f),
+  glm::vec3(-3.8f, -2.0f, -12.3f),
+  glm::vec3(2.4f, -0.4f, -3.5f),
+  glm::vec3(-1.7f,  3.0f, -7.5f),
+  glm::vec3(1.3f, -2.0f, -2.5f),
+  glm::vec3(1.5f,  2.0f, -2.5f),
+  glm::vec3(1.5f,  0.2f, -1.5f),
+  glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+#pragma endregion
+
+#pragma region Input Declare
+float lastX, lastY;
+bool firstMouse = true;
+void processInput(GLFWwindow* window) {
+	
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	//W AND S
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		camera.speedZ = 1.0f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		camera.speedZ = -1.0f;
+	}
+	else{
+		camera.speedZ = 0;
+	}
+
+	//A AND D
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		camera.speedX = 1.0f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		camera.speedX = -1.0f;
+	}
+	else {
+		camera.speedX = 0;
+	}
+
+	//Q AND E
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+	{
+		camera.speedY = -1.0f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+	{
+		camera.speedY = 1.0f;
+	}
+	else {
+		camera.speedY = 0;
+	}
+}
+
+void mouse_callback(GLFWwindow* window, double xPos, double yPos) {
+	if (firstMouse == true)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		firstMouse = false;
+	}
+	float deltaX, deltaY;
+	deltaX = xPos - lastX;
+	deltaY = yPos - lastY;
+
+	lastX = xPos;
+	lastY = yPos;
+
+	camera.ProcessMouseMovement(deltaX, deltaY);
+}
+#pragma endregion
+
+#pragma region LoadImageToGPU
+unsigned int LoadImageToGPU(const char* filename,GLint internalFormat,GLenum format,GLint textureSlot) {
+	unsigned int TexBuffer;
+	glGenTextures(1, &TexBuffer);
+	glActiveTexture(GL_TEXTURE0+textureSlot);
+	glBindTexture(GL_TEXTURE_2D, TexBuffer);
+
+	int width, height, nrChannel;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load(filename, &width, &height, &nrChannel, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("Load Error1\n");
+	}
+	stbi_image_free(data);
+	return TexBuffer;
+}
+#pragma endregion
+
+int main(int argc,char* argv[]) {
+	std::string exePath = argv[0];
+	//std::cout << exePath.substr(0,exePath.find_last_of('\\'))+"\\model\\nanosuit.obj"<< std::endl;
+
+	#pragma region Open Window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Opengl GLFW Window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Kirito's Opengl Game", NULL, NULL);
+	if (window == NULL)
+	{
+		printf("Open window failed");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+
+	//Init GLEW
+	glewExperimental = true;
+	if (glewInit()!=GLEW_OK)
+	{
+		printf("Init Glew Failed");
+		glfwTerminate();
+		return -1;
+	}
+
+	//Open the window
+	glViewport(0, 0, 800, 600);
+	glEnable(GL_DEPTH_TEST);
+#pragma endregion
+
+	#pragma region Init Shader Program
+	Shader* myShader = new Shader("vertexSource.txt", "fragmentSource.txt");
+#pragma endregion
+
+	#pragma region Init Material
+		//declare mymaterial
+	Material* myMaterial = new Material(myShader,
+			LoadImageToGPU("container2.png", GL_RGBA, GL_RGBA, Shader::DIFFUSE),
+			LoadImageToGPU("container2_specular.png", GL_RGBA, GL_RGBA, Shader::SPECULAR),
+			glm::vec3(1.0f,1.0f,1.0f),
+			64.0f);
+	#pragma endregion
+
+	#pragma region Init and Load Models to VAO and VBO
+	//Mesh cube(vertices);
+	Model model(exePath.substr(0, exePath.find_last_of('\\')) + "\\model\\nanosuit.obj");
+	//unsigned int VAO;
+	//glGenVertexArrays(1, &VAO);
+	//glBindVertexArray(VAO);
+	//
+	//unsigned int VBO;
+	//glGenBuffers(1, &VBO);
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//
+	//glVertexAttribPointer(6, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	//glEnableVertexAttribArray(6);
+	//glVertexAttribPointer(7, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(7);
+	//glVertexAttribPointer(8, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	//glEnableVertexAttribArray(8);
+
+#pragma endregion
+
+	#pragma region Init and load Textures
+		/*unsigned int TexBufferA;
+		TexBufferA = LoadImageToGPU("container.jpg",GL_RGB,GL_RGB,0);
+		unsigned int TexBufferB;
+		TexBufferB = LoadImageToGPU("awesomeface.png", GL_RGBA, GL_RGBA, 3);*/
+	#pragma endregion
+
+	#pragma region Prepare MVP matrices
+	//caculate transformation
+	glm::mat4 modelMat;
+	glm::mat4 viewMat;
+	glm::mat4 projMat;
+	projMat = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+#pragma endregion
+
+	#pragma region Render Loop	
+	//render loop
+	while (!glfwWindowShouldClose(window)) {
+		//input
+		processInput(window);
+
+		//clear Screen
+		glClearColor(0, 0, 0, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+		viewMat = camera.GetViewMatrix();
+
+		for (int i = 0; i < 1; i++)
+		{
+			//Set Model matrix
+			modelMat = glm::translate(glm::mat4(1.0f), cubePositions[i]);
+
+			//Set View and Projection Matrices here
+
+			//Set Material -> Shader Program
+			myShader->use();
+			//Set Material->Texture
+			//glActiveTexture(GL_TEXTURE0);
+			//glBindTexture(GL_TEXTURE_2D, myMaterial->diffuse);
+			//glActiveTexture(GL_TEXTURE1);
+			//glBindTexture(GL_TEXTURE_2D, myMaterial->specular);
+			//Set Material -> Uniforms
+			//glUniform1i(glGetUniformLocation(myShader->ID, "ourTexture"), 0);
+			//glUniform1i(glGetUniformLocation(myShader->ID, "ourFace"), 3);
+			glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat));
+			glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "viewMat"), 1, GL_FALSE, glm::value_ptr(viewMat));
+			glUniformMatrix4fv(glGetUniformLocation(myShader->ID, "projMat"), 1, GL_FALSE, glm::value_ptr(projMat));
+			glUniform3f(glGetUniformLocation(myShader->ID, "objColor"), 1.0f, 1.0f, 1.0f);
+			glUniform3f(glGetUniformLocation(myShader->ID, "ambientColor"), 0.1f, 0.1f, 0.1f);
+
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightD.pos"), lightD.position.x,lightD.position.y,lightD.position.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightD.color"),lightD.color.x,lightD.color.y,lightD.color.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightD.dirToLight"), lightD.direction.x, lightD.direction.y, lightD.direction.z);
+
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP0.pos"), lightP0.position.x, lightP0.position.y, lightP0.position.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP0.color"), lightP0.color.x, lightP0.color.y, lightP0.color.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP0.dirToLight"), lightP0.direction.x, lightP0.direction.y, lightP0.direction.z);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP0.constant"), lightP0.constant);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP0.linear"), lightP0.linear);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP0.constant"), lightP0.quadratic);
+
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP1.pos"), lightP1.position.x, lightP1.position.y, lightP1.position.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP1.color"), lightP1.color.x, lightP1.color.y, lightP1.color.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP1.dirToLight"), lightP1.direction.x, lightP1.direction.y, lightP1.direction.z);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP1.constant"), lightP1.constant);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP1.linear"), lightP1.linear);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP1.constant"), lightP1.quadratic);
+
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP2.pos"), lightP2.position.x, lightP2.position.y, lightP2.position.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP2.color"), lightP2.color.x, lightP2.color.y, lightP2.color.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP2.dirToLight"), lightP2.direction.x, lightP2.direction.y, lightP2.direction.z);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP2.constant"), lightP2.constant);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP2.linear"), lightP2.linear);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP2.constant"), lightP2.quadratic);
+
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP3.pos"), lightP3.position.x, lightP3.position.y, lightP3.position.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP3.color"), lightP3.color.x, lightP3.color.y, lightP3.color.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightP3.dirToLight"), lightP3.direction.x, lightP3.direction.y, lightP3.direction.z);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP3.constant"), lightP3.constant);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP3.linear"), lightP3.linear);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightP3.constant"), lightP3.quadratic);
+
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightS.pos"), lightS.position.x, lightS.position.y, lightS.position.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightS.color"), lightS.color.x, lightS.color.y, lightS.color.z);
+			glUniform3f(glGetUniformLocation(myShader->ID, "lightS.dirToLight"), lightS.direction.x, lightS.direction.y, lightS.direction.z);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightS.constant"), lightS.constant);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightS.linear"), lightS.linear);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightS.constant"), lightS.quadratic);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightS.CosPhyInner"), lightS.CosPhyInner);
+			glUniform1f(glGetUniformLocation(myShader->ID, "lightS.CosPhyOutter"), lightS.CosPhyOutter);
+
+			//reduce spot light
+			//glUniform1f(glGetUniformLocation(myShader->ID, "lightP.constant"), light.constant);
+			//glUniform1f(glGetUniformLocation(myShader->ID, "lightP.linear"), light.linear);
+			//glUniform1f(glGetUniformLocation(myShader->ID, "lightP.quadratic"), light.quadratic);
+			//reduce the out line
+			//glUniform1f(glGetUniformLocation(myShader->ID, "lightS.cosPhyInner"), light.cosPhyInner);
+			//glUniform1f(glGetUniformLocation(myShader->ID, "lightS.cosPhyOutter"), light.cosPhyOutter);
+
+			glUniform3f(glGetUniformLocation(myShader->ID, "cameraPos"), camera.Position.x,camera.Position.y,camera.Position.z);
+
+			myMaterial->shader->SetUniform3f("material.ambient", myMaterial->ambient);
+			//myMaterial->shader->SetUniform3f("material.diffuse", myMaterial->diffuse);
+			//myMaterial->shader->SetUniform3f("material.specular", myMaterial->specular);
+			myMaterial->shader->SetUniform1i("material.diffuse", Shader::DIFFUSE);
+			myMaterial->shader->SetUniform1i("material.specular", Shader::SPECULAR);
+			myMaterial->shader->SetUniform1f("material.shininess", myMaterial->shininess);
+			//Set Model
+			//glBindVertexArray(VAO);
+			//Drawcall
+			//glDrawArrays(GL_TRIANGLES, 0, 36);
+			//cube.Draw(myMaterial->shader);
+			model.Draw(myMaterial->shader);
+		}
+		//Clean buffers and prepare for next
+		glfwSwapBuffers(window);
+		glfwPollEvents();//get infor	
+		camera.UpdateCameraPos();
+	}
+#pragma endregion
+
+	//Exit
+	glfwTerminate();
+	return 0;
+}
