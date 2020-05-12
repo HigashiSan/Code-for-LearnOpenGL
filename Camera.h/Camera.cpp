dae83310409574707e44ebaa02f9e5c4e7175512
Kiritokojima <56297955+Kiritokojima@users.noreply.github.com>
2020-05-12 09:55:59 +0800

    Create Camera.cpp

---
+++ Camera.h/Camera.cpp
@@ -0,0 +1,71 @@
+#pragma once
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+class Camera
+{
+public:
+	Camera(glm::vec3 position, glm::vec3 target, glm::vec3 worldup);
+	Camera(glm::vec3 position, float pitch, float yaw, glm::vec3 worldup);
+
+	glm::vec3 Position;
+	glm::vec3 Forward;
+	glm::vec3 Right;
+	glm::vec3 Up;
+	glm::vec3 WorldUp;
+	float Pitch;
+	float Yaw;
+	float speedZ;
+	float speedX;
+	float speedY;
+
+	glm::mat4 GetViewMatrix();
+	void ProcessMouseMovement(float deltaX, float deltaY);
+	void UpdateCameraPos();
+private:
+	void UpdateCameraVectors();
+};
+
+#include "Camera.h"
+
+Camera::Camera(glm::vec3 position, glm::vec3 target, glm::vec3 worldup) {
+	Position = position;
+	WorldUp = worldup;
+	Forward = glm::normalize(target - position);
+	Right = glm::normalize(glm::cross(Forward, WorldUp));
+	Up = glm::normalize(glm::cross(Forward, Right));
+}
+
+Camera::Camera(glm::vec3 position, float pitch, float yaw, glm::vec3 worldup) {
+	Position = position;
+	WorldUp = worldup;
+	Pitch = pitch;
+	Yaw = yaw;
+	Forward.x = glm::cos(Pitch) * glm::sin(Yaw);
+	Forward.y = glm::sin(Pitch);
+	Forward.z = glm::cos(Pitch) * glm::cos(Yaw);
+	Right = glm::normalize(glm::cross(Forward, WorldUp));
+	Up = glm::normalize(glm::cross(Right, Forward));
+}
+
+glm::mat4 Camera::GetViewMatrix() {
+	return glm::lookAt(Position, Position + Forward, WorldUp);
+}
+
+void Camera::UpdateCameraVectors() {
+	Forward.x = glm::cos(Pitch) * glm::sin(Yaw);
+	Forward.y = glm::sin(Pitch);
+	Forward.z = glm::cos(Pitch) * glm::cos(Yaw);
+	Right = glm::normalize(glm::cross(Forward, WorldUp));
+	Up = glm::normalize(glm::cross(Right, Forward));
+}
+
+void Camera::ProcessMouseMovement(float deltaX, float deltaY) {
+	Pitch -= deltaY*0.001f;
+	Yaw -= deltaX*0.001f;
+	UpdateCameraVectors();
+}
+
+void Camera::UpdateCameraPos() {
+	Position += Forward * speedZ * 0.1f + Right * speedX * 0.1f, +Up * speedY * 0.1f;
+}
